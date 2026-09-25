6a6c4cdf9bd5810db14aa07a51e00bc0a6e77d32
Roberto <roberto.santana.berlin@gmail.com>
2026-09-25 11:24:13 +0200

    Weiter auf Raspi

--- files/app.sh
+++ files/app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/kolyma/Workspace/goldfisher.de/files || exit 1
+cd /home/petschora/Workspace/goldfisher.de/files || exit 1
 rm -f /run/flaskapp.sock
 ./venv/bin/gunicorn --workers 4 --threads 2 --bind unix:/run/flaskapp.sock --umask 007 --timeout 60 --bind 127.0.0.1:8005 app:app
 
--- files/requirements.sh
+++ files/requirements.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/venv/bin/activate"
+
+
 cd /home/kolyma/Workspace/gold
-source /home/kolyma/Workspace/goldfisher.de/files/venv/bin/activate
+#source /home/kolyma/Workspace/goldfisher.de/files/venv/bin/activate
+source "$SCRIPT_DIR/venv/bin/activate"
 pip install --upgrade pip
-pip install -r /home/kolyma/Workspace/goldfisher.de/files/requirements.txt
+pip install -r "$SCRIPT_DIR/Workspace/goldfisher.de/files/requirements.txt"
 deactivate
 
 # end-of-file
